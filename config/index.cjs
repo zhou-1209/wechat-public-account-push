338e46215a135b87d2cebdbc9a634c1e83afee30
zhou-1209 <107674655+zhou-1209@users.noreply.github.com>
2023-03-15 14:38:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,8 +6,6 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
-  USE_PASSAGE: 'push-plus',
   
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx92488ed406afcb4c',
@@ -23,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'e0161a47a24e42de900e94605af5bd48',
+      id: 'oBBfX5vnKY6Q4ihawt7H7G4ILJpw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
