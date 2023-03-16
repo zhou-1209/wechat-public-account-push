d743c1d6bc2468d39b5ddb0eacc026437b4c78a3
zhou-1209 <107674655+zhou-1209@users.noreply.github.com>
2023-03-16 10:12:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oBBfX5vnKY6Q4ihawt7H7G4ILJpw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'AhqrC55cXibVYkkcafXkMQbx0o5O1hSZDAfopPIiz-s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'FYnm-qOn7IHNMMakCQw9o_IWRr-CbeqxZR-A1yOmzlY',
 
   CALLBACK_USERS: [
     {
